1ae46ae55580a78043e4bf6e90b9ba45adbe86e0
Sami Honkasalo <N3227@student.jamk.fi>
2020-06-15 20:34:15 +0300

    moved main contents/routes to individual components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Layout from './layout/Layout';
 import CustomTheme from './layout/CustomTheme';
 import { UIProvider } from './store/ui/uiContext';
+import TestRoute from './TestRoute';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
-    <>
-      <UIProvider>
-        <CustomTheme>
-          <Router>
-            <div className="App">
-              <Layout />
-            </div>
-          </Router>
-        </CustomTheme>
-      </UIProvider>
-    </>
+    <UIProvider>
+      <CustomTheme>
+        <Router>
+          <div className="App">
+            <Layout>
+              <Switch>
+                <Route path="/" exact>
+                  <Dashboard />
+                </Route>
+                <Route path="/test" exact>
+                  <TestRoute />
+                </Route>
+              </Switch>
+            </Layout>
+          </div>
+        </Router>
+      </CustomTheme>
+    </UIProvider>
   );
 }
 
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,22 +2,16 @@ import React, { useEffect, useContext, useCallback } from 'react';
 import clsx from 'clsx';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { Switch, Route } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
 
 import AppBar from './AppBar';
 import SideDrawer from './SideDrawer';
 import { UIContext } from '../store/ui/uiContext';
 import { UITypes } from '../store/ui/uiReducer';
 import Notification from '../shared/components/ui/Notification';
-import useNotification from '../utils/hooks/useNotification';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -51,7 +45,6 @@ const Layout: React.FC = ({ children }) => {
     theme.breakpoints.down('md')
   );
   const { dispatch } = useContext(UIContext);
-  const notify = useNotification();
 
   const handleDrawerOpen = useCallback(() => {
     dispatch({ type: UITypes.OPEN_SIDEDRAWER });
@@ -69,21 +62,6 @@ const Layout: React.FC = ({ children }) => {
     }
   }, [isMedium, handleDrawerClose, handleDrawerOpen]);
 
-  // TEST FUNCTION FOR NOTIFICATIONS
-  const handleNotificationTest = () => {
-    notify({ message: 'Test notification #1 - normal/info' });
-    notify({
-      message: 'Test notification #2 - warning',
-      type: 'warning',
-      hideDuration: 5000,
-    });
-    notify({
-      message: 'Test notification #3 - error',
-      type: 'error',
-      hideDuration: 2000,
-    });
-  };
-
   return (
     <div className={classes.root}>
       <AppBar />
@@ -92,62 +70,7 @@ const Layout: React.FC = ({ children }) => {
         <div className={classes.appBarSpacer} />
         {/* TESTING CONTENT */}
         <Switch>
-          <Route path="/" exact>
-            <Container maxWidth="lg" className={classes.container}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} md={8} lg={9}>
-                  <Paper className={fixedHeightPaper}>Things</Paper>
-                </Grid>
-                <Grid item xs={12} md={4} lg={3}>
-                  <Paper className={fixedHeightPaper}>Other things</Paper>
-                </Grid>
-                <Grid item xs={12}>
-                  <Paper className={classes.paper}>
-                    More things
-                    <Grid container spacing={3}>
-                      <Grid item xs={4}>
-                        <Button variant="contained">contained</Button>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Fab color="secondary" aria-label="remove">
-                          <RemoveIcon />
-                        </Fab>
-                        <Button variant="contained" color="primary">
-                          contained primary
-                        </Button>
-                        <Fab
-                          color="primary"
-                          aria-label="add"
-                          onClick={handleNotificationTest}
-                        >
-                          <AddIcon />
-                        </Fab>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Button variant="contained" color="secondary">
-                          contained secondary
-                        </Button>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Button variant="outlined">outlined</Button>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Button variant="outlined" color="primary">
-                          outlined primary
-                        </Button>
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Button variant="outlined" color="secondary">
-                          outlined secondary
-                        </Button>
-                      </Grid>
-                    </Grid>
-                  </Paper>
-                </Grid>
-              </Grid>
-              <Box pt={4}>And even more...</Box>
-            </Container>
-          </Route>
+          <Route path="/" exact />
           <Route path="/test" exact>
             <Container maxWidth="lg" className={classes.container}>
               <Grid container spacing={3}>
